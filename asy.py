edce128f9b7d146ec9321dcb5bbeed15e446a5ad
Sererim <danielleymanw@gmail.com>
2023-09-17 16:12:23 +0300

    Final commit for async downloader.

--- asy.py
+++ asy.py
@@ -1,9 +1,40 @@
 import asyncio
+import requests
+import time
+from PIL import Image
+from io import BytesIO
+from pars import parser
 
 
-async def main():
-    pass
+async def download(url: str):
+    try:
+        req = requests.get(url)
+        img = Image.open(BytesIO(req.content))
+        img.save(url.split('/')[-1])
+        print(f"Downloaded from url - {url}\n"
+              f"In {time.time() - start_time:.2f} seconds")
+    except Exception as e:
+        print(f"Exception: {e}\nURL: {url}")
 
+
+async def main(urls: list[str]):
+    tasks = []
+    
+    for url in urls:
+        task = asyncio.ensure_future(download(url))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+
+start_time = time.time()
 
 if __name__ == "__main__":
-    pass
+    args = parser.parse_args()
+    try:
+        loop = asyncio.get_running_loop()
+    except:
+        loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        asyncio.run(main(args.urls))
+    except KeyboardInterrupt:
+        pass
