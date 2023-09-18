844ae795b5d9b83ffb4dee24d0592d2e74bd8092
Sererim <danielleymanw@gmail.com>
2023-09-18 18:38:10 +0300

    Final commit for multithread && multiprocessing downloader.

---
+++ multi.py
@@ -0,0 +1,45 @@
+import time
+import requests
+import concurrent.futures
+from PIL import Image
+from io import BytesIO
+from multiprocessing import Process
+from pars import parser
+
+
+start_time = time.time()
+
+
+def download_image(url: str):
+    try:
+        req = requests.get(url)
+        img = Image.open(BytesIO(req.content))
+        img.save(url.split('/')[-1])
+        print(f"Downloaded from url - {url}\n"
+              f"In {time.time() - start_time:.2f} seconds")
+    except Exception as e:
+        print(f"Exception: {e}\nURL: {url}")
+
+
+def thread_tasks(urls: list[str]):
+    with concurrent.futures.ThreadPoolExecutor() as exe:
+        exe.map(download_image, urls)
+
+
+def process_task(url_chunks):
+    
+    processes = []
+    
+    for urls in url_chunks:
+        proc = Process(target=thread_tasks, args=(urls, ))
+        processes.append(proc)
+        proc.start()
+    
+    for proc in processes:
+        proc.join()
+
+
+if __name__ == "__main__":
+    urls = parser.parse_args().urls
+    url_chunks = [urls[i:i + 5] for i in range(0, len(urls), 5)]
+    process_task(url_chunks)
