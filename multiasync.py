700b5d5cac9160bac87323073c22ccbd90093fc6
Sererim <danielleymanw@gmail.com>
2023-09-18 18:49:31 +0300

    Final commit for multiprocessing async downloader.

---
+++ multiasync.py
@@ -0,0 +1,43 @@
+import requests
+import time
+import multiprocessing
+import asyncio
+from PIL import Image
+from io import BytesIO
+from pars import parser
+
+
+start_time = time.time()
+
+
+async def download_image(url: str):
+    try:
+        req = requests.get(url)
+        img = Image.open(BytesIO(req.content))
+        img.save(url.split('/')[-1])
+        print(f"Downloaded from url - {url}\n"
+              f"In {time.time() - start_time:.2f} seconds")
+    except Exception as e:
+        print(f"Exception: {e}\nURL: {url}")
+
+
+async def multiproceamain(urls: list[str]):
+    manager = multiprocessing.Manager()
+    queue = manager.Queue()
+    
+    for url in urls:
+        task = asyncio.create_task(download_image(url))
+
+
+if __name__ == "__main__":
+    urls = parser.parse_args().urls
+    try:
+        loop = asyncio.get_running_loop()
+    except:
+        loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        asyncio.run(multiproceamain(urls))
+    except KeyboardInterrupt:
+        pass
+    
