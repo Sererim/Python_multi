d718aba88d4ea2078a6e491e3c66bb400fa4be4f
Sererim <danielleymanw@gmail.com>
2023-09-18 18:19:05 +0300

    Final commit for multithread image downloader.

---
+++ multithread.py
@@ -0,0 +1,36 @@
+import threading
+import time
+import requests
+from PIL import Image
+from io import BytesIO
+from pars import parser
+
+start_time = time.time()
+
+def download_image(url: str):
+    try:
+        req = requests.get(url)
+        img = Image.open(BytesIO(req.content))
+        img.save(url.split('/')[-1])
+        print(f"Downloaded from url - {url}\n"
+              f"In {time.time() - start_time:.2f} seconds")
+    except Exception as e:
+        print(f"Exception: {e}\nURL: {url}")
+
+
+def thmain(urls: list[str]):
+    threads = []
+    
+    for url in urls:
+        thread = threading.Thread(target=download_image, args=[url])
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    thmain(args.urls)
+ 
